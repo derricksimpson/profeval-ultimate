02a7b1e76c0cff3d7fe003eecedbd2ef457c91a8
derricksimpson <derrick.simpson@gmail.com>
2024-07-05 17:39:57 -0400

    refactor: Cache subjects for faster retrieval in getAllSubjects

--- src/service/evaluation-service.ts
+++ src/service/evaluation-service.ts
@@ -99,13 +99,22 @@ export const getSearchResults = async (Astro, query: string) => {
 
 export const getAllSubjects = async (Astro, schoolId: number) => {
   const DB = Astro.locals.runtime.env.DB;
+
+  const KV = Astro.locals.runtime.env.profeval;
+
+  let value = await KV.get(`subjects_${schoolId}`);
+  if (value) {
+    console.log('FOUND in kv!');
+    return value.split(',').map((subject) => ({ Subject: subject }));
+  }
+
   //let query = 'SELECT distinct Subject from Evaluations where schoolId = ? order by Subject asc';
   let query = 'select * from aggregated_subjects where SchoolID = ?';
   const response = await DB.prepare(query).bind(schoolId).all();
 
   if (response.results.length > 0) {
     let subjects = response.results[0].Subjects.split(',');
-
+    await KV.put(`subjects_${schoolId}`, response.results[0].Subjects);
     return subjects.map((subject) => ({ Subject: subject }));
   }
 };
