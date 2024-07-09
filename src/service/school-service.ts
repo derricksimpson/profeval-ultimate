6fe916ba846cce9c7bf1277c59a25364734312c6
derricksimpson <derrick.simpson@gmail.com>
2024-07-09 07:48:35 -0400

    feat: Update school-service to add getSchoolById and getSchoolsByState functions

---
+++ src/service/school-service.ts
@@ -0,0 +1,42 @@
+import { School } from '~/models/School';
+import { measureQuery } from './metrics-service';
+
+export const getSchoolById = async (context, id: number): Promise<School> => {
+  if (id > 0) {
+    const DB = context.locals.runtime.env.DB;
+    const db = DB;
+    let query = `SELECT * from schools where id = ?`;
+
+    let response = await DB.prepare(query).bind(id).all();
+
+    await measureQuery(db, { query, response }, 'getSchoolById');
+
+    let school = response?.results[0] as School;
+
+    return School.fromRaw(school);
+  }
+  return Promise.resolve(null);
+};
+
+/**
+ *
+ * @param context
+ * @param state
+ * @returns
+ */
+export const getSchoolsByState = async (context, state: string): Promise<School[]> => {
+  if (state) {
+    const DB = context.locals.runtime.env.DB;
+    const db = DB;
+    let query = `SELECT * from schools where RegionId = ?`;
+
+    let response = await DB.prepare(query).bind(state).all();
+
+    await measureQuery(db, { query, response }, 'getSchoolsByState');
+
+    let schools = response?.results as School[];
+
+    return schools.map((school) => School.fromRaw(school));
+  }
+  return Promise.resolve([]);
+};
