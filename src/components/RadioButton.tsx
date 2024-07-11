e223972b265e101834178ceb148bdc762098bb10
derricksimpson <derrick.simpson@gmail.com>
2024-07-11 07:59:22 -0400

    refactor: Update RadioButton component to include name, width, colors, and options props

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,7 +1,7 @@
 type RadioButtonProps = {
-  name: string;
-  label: string;
-  width: number;
+  name?: string;
+  label?: string;
+  width?: number;
   colors?: string[];
   options?: string[];
   onChange?: (name: string, value: string) => void;
@@ -12,15 +12,11 @@ export default ({ name, label, colors, options, width, onChange }: RadioButtonPr
     name = 'radioGroup' + Math.random() * 1000000;
   }
   if (!options) {
-    options = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6', 'Option 7'];
+    options = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'];
   }
 
   if (!colors) {
     colors = ['#2563EB', '#777777', '#8B5CF6', '#DB2777', '#475569', '#EA580C'];
-  }
-
-  if (!width) {
-    width = 32;
   }
 
   if (!onChange) {
@@ -45,43 +41,46 @@ export default ({ name, label, colors, options, width, onChange }: RadioButtonPr
     <div className="mx-auto px-4">
       {label && <h2 className="text-gray-800 font-medium">{label}</h2>}
       <ul className="mt-4 flex items-center flex-wrap gap-4">
-        {options.map((item, idx) => (
-          <li key={idx} className="flex-none ">
-            <label
-              htmlFor={`${instId}_${idx}`}
-              className="flex items-center space-x-2 cursor-pointer"
-              style={{ userSelect: 'none' }}
-            >
-              <div className="relative">
-                <input
-                  onChange={() => onChange(name, item)}
-                  id={`${instId}_${idx}`}
-                  type="radio"
-                  name={`${instId}_group`}
-                  className="sr-only peer"
-                />
-                <div
-                  className={`w-8 h-8 border-2 rounded-full peer-checked:ring ring-offset-2 ${colorMap[idx].ring}`}
-                ></div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none peer-checked:block peer-checked:opacity-100 opacity-0 transition-opacity duration-0"
-                >
-                  <path
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="3"
-                    d="M4.5 12.75l6 6 9-13.5"
-                    stroke={colorMap[idx].base}
+        {options.map((item, idx) => {
+          let w = width ? `${width}em` : '';
+          return (
+            <li key={idx} className={`flex-none inline-block}`} style={{ width: w }}>
+              <label
+                htmlFor={`${instId}_${idx}`}
+                className={`flex items-center space-x-2 cursor-pointer`}
+                style={{ userSelect: 'none' }}
+              >
+                <div className="relative">
+                  <input
+                    onChange={() => onChange(name, item)}
+                    id={`${instId}_${idx}`}
+                    type="radio"
+                    name={`${instId}_group`}
+                    className="sr-only peer"
                   />
-                </svg>
-              </div>
-              <span className="inline-block font-medium pr-5">{item}</span>
-            </label>
-          </li>
-        ))}
+                  <div
+                    className={`w-8 h-8 border-2 rounded-full peer-checked:ring ring-offset-2 ${colorMap[idx].ring}`}
+                  ></div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="w-5 h-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none peer-checked:block peer-checked:opacity-100 opacity-0 transition-opacity duration-0"
+                  >
+                    <path
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="3"
+                      d="M4.5 12.75l6 6 9-13.5"
+                      stroke={colorMap[idx].base}
+                    />
+                  </svg>
+                </div>
+                <span className="inline-block font-medium pr-5">{item}</span>
+              </label>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
